de4887a1dac31b54f0d62bd25071b6f210afaed9
Alan Yeh <alan@yerl.cn>
2016-08-18 09:45:31 +0800

    Update macros

--- AYRecord.podspec
+++ AYRecord.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AYRecord'
-  s.version          = '1.0.0'
+  s.version          = '1.0.1'
   s.summary          = 'Fast and convenient ORM framework, that is AYRecord.'
 
   s.homepage         = 'https://github.com/alan-yeh/AYRecord'
--- AYRecord/Classes/macros/convenientmacros.h
+++ AYRecord/Classes/macros/convenientmacros.h
@@ -5,14 +5,13 @@
 //  Copyright © 2015年 Alan Yeh. All rights reserved.
 //
 
-#ifndef AY_CONVENIENT_MACROS_H
-#define AY_CONVENIENT_MACROS_H
+#ifndef AY_AYRECORD_CONVENIENT_MACROS_H
+#define AY_AYRECORD_CONVENIENT_MACROS_H
 
 #define returnValIf(condition, val) if (!!(condition)){ return val;}
 #define returnIf(condition)         if (!!(condition)){ return;    }
 #define breakIf(condition)          if (!!(condition)){ break;     }
 #define continueIf(condition)       if (!!(condition)){ continue;  }
-
 
 #define doIf(condition, action) \
    do {                         \
@@ -20,14 +19,5 @@
          action;                \
       }                         \
    } while(0)
-
-//消除[NSObject performSelector:]引起的警告
-#define SuppressPerformSelectorLeakWarning(action)                        \
-   do {                                                                   \
-      _Pragma("clang diagnostic push")                                    \
-      _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
-      action;                                                             \
-      _Pragma("clang diagnostic pop")                                     \
-   } while (0)
 
 #endif
